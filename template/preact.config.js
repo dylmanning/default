cecf4a44f8f4f056ce1aa46afab29c1e15dc3cf7
Dylan Manning <dyl.manning@gmail.com>
2022-05-23 07:20:07 +0000

    cleanup preact.config.js

--- template/preact.config.js
+++ template/preact.config.js
@@ -1,9 +1,9 @@
 export default (config, env) => {
-  //absolute paths
+  // absolute paths
   config.resolve.modules.push(env.src)
   config.resolve.modules.push(`${env.cwd}/node_modules`)
 
-  //disable sourcemaps
+  // disable sourcemaps
   if (env.isProd) {
     config.devtool = false;
   }
